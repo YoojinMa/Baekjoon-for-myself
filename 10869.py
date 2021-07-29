a0074b9cd5f8414c975bbf6b3f5e3207eddc2ca7
YoojinMa <geronimo9595@naver.com>
2021-07-29 23:56:03 +0900

    [Solved] 10869

---
+++ 10869.py
@@ -0,0 +1,16 @@
+'''
+문제 - 입출력과 사칙연산
+백준 10869 사칙연산
+https://www.acmicpc.net/problem/10869
+'''
+
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+
+'''
+알고 가자
+'''
