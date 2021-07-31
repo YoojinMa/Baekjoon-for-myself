a568428f002aa3dfa8ee4cd5e4ab643100ea0559
YoojinMa <geronimo9595@naver.com>
2021-08-01 00:00:26 +0900

    [Solved] 1330

---
+++ 1330.py
@@ -0,0 +1,19 @@
+'''
+문제 - if문
+백준 1330 두 수 비교하기
+https://www.acmicpc.net/problem/1330
+'''
+
+
+a, b = map(int, input().split())
+if a > b:
+    print(">")
+elif a < b:
+    print("<")
+else:
+    print("==")
+
+
+'''
+알고 가자
+'''
