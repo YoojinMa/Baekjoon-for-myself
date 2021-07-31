923ec2581048f0b0d83739a7611458836b470596
YoojinMa <geronimo9595@naver.com>
2021-07-31 23:52:41 +0900

    [Solved] 2588

---
+++ 2588.py
@@ -0,0 +1,18 @@
+'''
+문제 - 입출력과 사칙연산
+백준 2588 곱셈
+https://www.acmicpc.net/problem/2588
+'''
+
+
+a = int(input())
+b = input()
+print(a * int(b[2]))
+print(a * int(b[1]))
+print(a * int(b[0]))
+print(a * int(b))
+
+
+'''
+알고 가자
+'''
