15ef82b45819c4c9b007dfefd54d4b5d1b8d2ce6
YoojinMa <geronimo9595@naver.com>
2021-08-25 00:03:47 +0900

    [Solved] 2742

---
+++ 2742.py
@@ -0,0 +1,17 @@
+'''
+문제 - for문
+백준 2742 기찍 N
+https://www.acmicpc.net/problem/2742
+'''
+
+
+n = int(input())
+for i in range(n, 0, -1):
+    print(i)
+
+
+'''
+알고 가자
+for i in range(n, 0, -1) : range(초기값, 종료값, 증가값)
+    종료값은 본인을 포함하지 않으므로 0으로 설
+'''
